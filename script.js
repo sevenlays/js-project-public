c28a2cdf9ec7149c7f3ee01066325ee310dd7870
Vlados <7evenLays@gmail.com>
2022-02-17 15:39:06 +0200

    new script movies

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-console.log('hi');
-console.log('hello again');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотреных фильмов', ''),
+        b = prompt('На сколько оцениваите его?', ''),
+            c = prompt('Один из последних просмотреных фильмов', ''),
+                d = prompt('На сколько оцениваите его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
