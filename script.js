9238088d822f9a03eae6c9084ecd52b5554cc2f9
Vlados <7evenLays@gmail.com>
2022-02-19 16:07:54 +0200

    Movies v2

--- script.js
+++ script.js
@@ -8,13 +8,30 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотреных фильмов', ''),
-        b = prompt('На сколько оцениваите его?', ''),
-            c = prompt('Один из последних просмотреных фильмов', ''),
-                d = prompt('На сколько оцениваите его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотреных фильмов', ''),
+          b = prompt('На сколько оцениваите его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+          personalMovieDB.movies[a] = b;
+
+          if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done');
+          } else {
+              console.log('error');
+              i--
+          }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Ошибочка');
+}
 
 console.log(personalMovieDB);
 
